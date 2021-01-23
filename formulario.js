66274f3fb2b6ac174fe078c86d8a73fd931f8438
Giovanni Di Rosa <giodirosaartes@gmail.com>
2021-01-23 11:36:50 -0300

    avance de object

--- formulario.js
+++ formulario.js
@@ -1,39 +1,55 @@
 // alert("hola a todos");
 
-let btn_excelente = document.getElementById("mi-boton");
-let btn_muybueno = document.getElementById("mi-boton1");
-let btn_bueno = document.getElementById("mi-boton2");
-let btn_mala = document.getElementById("mi-boton3");
+let btn = document.getElementById("1");
 
-btn_excelente.addEventListener("click", btnLike);
-btn_muybueno.addEventListener("click", btnLike);
-btn_bueno.addEventListener("click", btnLike);
+btn.addEventListener("click", accionar);
+
+function accionar() {
+  let loquesea = document.getElementById("1");
+
+  console.log(loquesea.name);
+  alert(loquesea.name);
 
 
-function btnLike() {
-  // let seleccion= document.getElementsByClassName("estrella");
-
-  let excelente = document.getElementById("mi-boton");
-  let muyBueno = document.getElementById("mi-boton1");
-  let bueno = document.getElementById("mi-boton2");
-  let mala = document.getElementById("mi-boton3");
-
-  if (excelente !== btn_excelente) {
-    document.getElementById("mi-boton").style.color = "yellow";
-    alert("caigo en true");
-    document.getElementById("mi-boton1").style.color= "red";
-  } else if (muyBueno === btn_muybueno) {
-    document.getElementById("mi-boton1").style.color = "yellow";
-    // document.getElementById("mi-boton1").style.color = "green";
-    alert("pasa algo");
-
-  } else if (bueno === btn_bueno ){
-    document.getElementById("mi-boton2").style.color = "yellow";
-
-  } else {
-    alert("no funciono");
-  }
 }
+
+// let btn_excelente = document.getElementById("mi-boton");
+// let btn_muybueno = document.getElementById("mi-boton1");
+// let btn_bueno = document.getElementById("mi-boton2");
+// let btn_mala = document.getElementById("mi-boton3");
+
+// btn_excelente.addEventListener("click", btnLike);
+// btn_muybueno.addEventListener("click", btnLike);
+// btn_bueno.addEventListener("click", btnLike);
+
+// function btnLike() {
+//   let seleccion= document.getElementsByClassName("estrella");
+
+//   let excelente = document.getElementById("mi-boton");
+//   let muyBueno = document.getElementById("mi-boton1");
+//   let bueno = document.getElementById("mi-boton2");
+//   let mala = document.getElementById("mi-boton3");
+
+//   alert(excelente)
+//   alert(btn_excelente)
+//   alert(muyBueno)
+//   alert(btn_muybueno)
+//   if (excelente == btn_excelente) {
+//     document.getElementById("mi-boton").style.color = "yellow";
+//     alert("caigo en true");
+//     document.getElementById("mi-boton1").style.color= "red";
+//   } else if (muyBueno == btn_muybueno) {
+//     document.getElementById("mi-boton1").style.color = "yellow";
+//     document.getElementById("mi-boton1").style.color = "green";
+//     alert("pasa algo");
+
+//   } else if (bueno == btn_bueno ){
+//     document.getElementById("mi-boton2").style.color = "yellow";
+
+//   } else {
+//     alert("no funciono");
+//   }
+// }
 
 // function verSeleccion() {
 
