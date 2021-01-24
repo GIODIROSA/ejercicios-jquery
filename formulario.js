49edc3830ccc2f4bfcc02598beb9e7c8620c86ef
Giovanni Di Rosa <giodirosaartes@gmail.com>
2021-01-24 10:24:38 -0300

    funcionamiento btn estrellas

--- formulario.js
+++ formulario.js
@@ -11,121 +11,33 @@ btn4.addEventListener("click", wololo);
 btn5.addEventListener("click", cinco);
 
 function accionar() {
-  if (btn2.id == 2) {
-    alert(btn2.id);
-  } else if (btn3.id == 3) {
-    alert(btn3.id);
-  } else {
-    alert("estas mal");
-  }
+  alert(btn2.id);
+  document.getElementById("2").style.color = "yellow";
+  document.getElementById("3").style.color = "gray";
+  document.getElementById("4").style.color = "gray";
+  document.getElementById("5").style.color = "gray";
 }
 
 function loquesea() {
   alert(btn3.id);
+  document.getElementById("2").style.color = "gray";
+  document.getElementById("3").style.color = "yellow";
+  document.getElementById("4").style.color = "gray";
+  document.getElementById("5").style.color = "gray";
 }
 
 function wololo() {
   alert(btn4.id);
+  document.getElementById("2").style.color = "gray";
+  document.getElementById("3").style.color = "gray";
+  document.getElementById("4").style.color = "yellow";
+  document.getElementById("5").style.color = "gray";
 }
 
 function cinco() {
   alert(btn5.id);
+  document.getElementById("2").style.color = "gray";
+  document.getElementById("3").style.color = "gray";
+  document.getElementById("4").style.color = "gray";
+  document.getElementById("5").style.color = "yellow";
 }
-
-// let btn_excelente = document.getElementById("mi-boton");
-// let btn_muybueno = document.getElementById("mi-boton1");
-// let btn_bueno = document.getElementById("mi-boton2");
-// let btn_mala = document.getElementById("mi-boton3");
-
-// btn_excelente.addEventListener("click", btnLike);
-// btn_muybueno.addEventListener("click", btnLike);
-// btn_bueno.addEventListener("click", btnLike);
-
-// function btnLike() {
-//   let seleccion= document.getElementsByClassName("estrella");
-
-//   let excelente = document.getElementById("mi-boton");
-//   let muyBueno = document.getElementById("mi-boton1");
-//   let bueno = document.getElementById("mi-boton2");
-//   let mala = document.getElementById("mi-boton3");
-
-//   alert(excelente)
-//   alert(btn_excelente)
-//   alert(muyBueno)
-//   alert(btn_muybueno)
-//   if (excelente == btn_excelente) {
-//     document.getElementById("mi-boton").style.color = "yellow";
-//     alert("caigo en true");
-//     document.getElementById("mi-boton1").style.color= "red";
-//   } else if (muyBueno == btn_muybueno) {
-//     document.getElementById("mi-boton1").style.color = "yellow";
-//     document.getElementById("mi-boton1").style.color = "green";
-//     alert("pasa algo");
-
-//   } else if (bueno == btn_bueno ){
-//     document.getElementById("mi-boton2").style.color = "yellow";
-
-//   } else {
-//     alert("no funciono");
-//   }
-// }
-
-// function verSeleccion() {
-
-//     var modelo = document.getElementById('modelo');
-
-//     var color = document.getElementById('color');
-
-//     var enviar = document.getElementById('enviar');
-
-//     var seleccionado = document.getElementById("selector").value;
-
-//     if (seleccionado == 'modelo') {
-
-//         modelo.style.display = 'block';
-
-//         color.style.display = 'none';
-
-//         enviar.style.display = 'block';
-
-//     } else if (seleccionado == 'color') {
-
-//         modelo.style.display = 'none';
-
-//         color.style.display = 'block';
-
-//         enviar.style.display = 'block';
-
-//     } else {
-
-//         modelo.style.display = 'none';
-
-//         color.style.display = 'none';
-
-//         enviar.style.display = 'none';
-
-//     }
-
-// }
-
-// console.log(papo.id);
-//   alert(papo.id);
-//   document.getElementById("2").style.color = "yellow";
-//   document.getElementById("3").style.color = "green";
-//   document.getElementById("4").style.color = "red";
-//   document.getElementById("5").style.color = "red";
-
-// document.getElementById("3").style.color = "yellow";
-// document.getElementById("2").style.color = "red";
-// document.getElementById("4").style.color = "red";
-
-// console.log(excelente.id);
-//   alert(papo.id);
-
-//   if (muyBueno.id == 3) {
-//     alert(muyBueno.id);
-//   } else if (excelente.id == 2) {
-//     alert(excelente.id);
-//   } else {
-//     alert("estas cayendo en el else");
-//   }
